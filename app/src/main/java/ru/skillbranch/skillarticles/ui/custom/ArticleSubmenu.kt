b7a8d9f3108f201c61d248ad42b53f058791f64e
Andreev Denis <andreev.denis@gmail.com>
2019-12-25 01:57:29 +0300

    52:30

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
@@ -4,15 +4,20 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.shape.MaterialShapeDrawable
 import ru.skillbranch.skillarticles.R
 
-class Submenu @JvmOverloads constructor(
+class ArticleSubmenu @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr){
     init {
         View.inflate(context, R.layout.layout_submenu, this)
+
+        val materialBg = MaterialShapeDrawable.createWithElevationOverlay(context)
+        materialBg.elevation = elevation
+        background = materialBg
     }
 
 }
