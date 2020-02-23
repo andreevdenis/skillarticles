4455980743821daaea3c64b946ded5f485de755e
Andreev Denis <andreev.denis@gmail.com>
2020-02-24 02:57:16 +0300

    setMarginOptionally

---
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -0,0 +1,20 @@
+package ru.skillbranch.skillarticles.extensions
+
+import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
+
+fun View.setMarginOptionally
+            (left:Int = marginLeft,
+             top : Int = marginTop,
+             right : Int = marginRight,
+             bottom : Int = marginBottom) {
+    if (this.getLayoutParams() is MarginLayoutParams) {
+        val p = this.getLayoutParams() as MarginLayoutParams
+        p.setMargins(left, top, right, bottom)
+        this.requestLayout()
+    }
+}
