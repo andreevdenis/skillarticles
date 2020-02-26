40e31f52b3c91b8fe99875cfe697a318f68bf946
andreevdenis <andreev.denis@gmail.com>
2020-02-26 20:32:19 +0300

    indexesOf fix

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,15 +1,17 @@
 package ru.skillbranch.skillarticles.extensions
 
-fun String.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
+fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
     val result = mutableListOf<Int>()
     val substrLen = substr.length
     var  startindex = 0
     do {
-        val index = this.indexOf(substr, startindex, ignoreCase)
-        if (index != -1) {
-            result.add(index)
+        var gotIndex = false
+        val index = this?.indexOf(substr, startindex, ignoreCase)
+        if (index != null && index != -1) {
+            result.add(index!!)
             startindex = index + substrLen
+            gotIndex = true
         }
-    } while (index != -1)
+    } while (gotIndex)
     return result
 }
